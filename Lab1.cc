7ba553405e83e0f364cc175cdc2363fe577e6f45
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-02-11 23:18:36 -0500

    Create Lab1.cc

---
+++ Lab1.cc
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    
+    printf("Enter an odd number: \n");
+    scanf("%d", &num);
+    
+    if (num % 2 == 0){
+        printf("Error! That is not an odd number.\n");
+    }
+    else {
+        for(int row = 0; row <num; row++){
+            int star = 2 * row + 1;
+            int indent = num - row - 1;
+            
+            for(int i = 0; i<indent; i++)
+            printf(" ");
+            
+            for (int i = 0; i < star; i++) 
+            printf("*");
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
