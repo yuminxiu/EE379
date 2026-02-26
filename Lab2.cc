736536d99d07994c488a8b751387b9d43dc4dd22
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-02-26 12:00:08 -0500

    Update Lab2.cc

--- Lab2.cc
+++ Lab2.cc
@@ -18,7 +18,7 @@
 
 int main() {
 	XGpio output;							
-
+	XGpio input;
 	int led_val = 0;
 	int count = 0;
 	int mode = 0; // 0..3
@@ -26,73 +26,83 @@ int main() {
 	int all;
 	int speed = 0;
 	int delay;
+	int switch_data;
 
   XGpio_Initialize(&input, XPAR_AXI_GPIO_0_DEVICE_ID); // initialize input XGpio variable
-	XGpio_Initialize(&output, XPAR_AXI_GPIO_1_DEVICE_ID);	// initialize output XGpio variable
+  XGpio_Initialize(&output, XPAR_AXI_GPIO_1_DEVICE_ID);	// initialize output XGpio variable
 
 
-  XGpio_SetDataDirection(&input, 1, 0xF);  // Swtich inputs
-	XGpio_SetDataDirection(&output, 1, 0x0);		        // LED outputs, 0 = output 1 = input
+  XGpio_SetDataDirection(&input, 1, 0xF);  // Switch inputs
+  XGpio_SetDataDirection(&output, 1, 0x0);		        // LED outputs, 0 = output 1 = input
 	
+
   while (true) {
     switch_data = XGpio_DiscreteRead(&input, 1);
-	  if (switch_data & 0x01){
+	  if (switch_data & 0x1){
 		  mode++;
+	  }
 		  
-		  if (mode == 4)
-			  mode = 0;
+	  if (mode > 4){
+		  mode = 0;
 		  led_val = 0;
 		  all = 0;
 		  dir = 1;
-		  XGpio_DiscreteRead(&input,1) &0x01); // turn on LED
 	  }
 	  if (switch_data & 0x02) {
 		  speed ++;
-		  
-	  if (speed == 3)
+	  }
+	  if (speed > 3){
 		  speed = 0;
-		  XGpio_DiscreteRead(&input,1) &0x02);
 	  }
 	switch(speed){
-		case 0: delay = ONE_SEC / 4;
-		case 1: delay = ONE_SEC / 2;
-		case 2: delay = ONE_SEC / 10;
-		case 3: delay = ONE_SEC * 2;
-		default: delay = ONE_SEC;
+		case 0: delay = ONE_SEC / 4; break;
+		case 1: delay = ONE_SEC / 2; break;
+		case 2: delay = ONE_SEC / 10; break;
+		case 3: delay = ONE_SEC * 2; break;
+		// default: delay = ONE_SEC; break;
 	}
-	  switch(mode){
-		  case 0: // Mode 1: Left -> Right, wrap
-			  XGpio_DiscreteWrite(&output,1 ,(1<< led_val));
-			  led_val++;
-			  if (led_val == 8)
-				  led_val = 0;
-			  break;
 
-		  case 1: // Mode 2: Right -> Left, wrap
+	switch(mode){
+	  case 0: // Mode 1: Left -> Right, wrap
+		  for (int i = 3; i >= 0; i--){
+			  led_val = (1 << i);
+			  XGpio_DiscreteWrite(&output,1 ,led_val);
+			  for (count = 0; count < delay; count++);
+		  }
+		  break;
 
-			  XGpio_DiscreteWrite(&output, 1, (1 << led_val));
-			  led_val --;
-			  if (led_val < 0)
-				  led_val = 7;
-			  break;
+	  case 1: // Mode 2: Right -> Left, wrap
+		  for (int i = 0; i < 4; i++){
+			  led_val = (1 << i);
+			  XGpio_DiscreteWrite(&output,1 ,led_val);
+			  for (count = 0; count < delay; count++);
+		  }
+		  break;
 
-		  case 2: // Mode 3: Back and forth
-			  XGpio_DiscreteWrite(&output,1 , (1 << led_val);
-			  led_val += dir;
-			  if (led_val == 7)
-				  dir = -1;
-			  if (led_val == 0)
-				  dir = 1;
-			  break;
+	  case 2: // Mode 3: Back and forth
+		  for (int i = 3; i >= 0; i--){
+			  led_val = (1 << i);
+			  XGpio_DiscreteWrite(&output,1 ,led_val);
+			  for (count = 0; count < delay; count++);
+		  }
+		  for (int i = 0; i < 4; i++){
+			  led_val = (1 << i);
+			  XGpio_DiscreteWrite(&output,1 ,led_val);
+			  for (count = 0; count < delay; count++);
+		  }
+		  break;
 
-		  case 3: // All 4 together
-			  all = !all;
-			  if (all) {
-			  XGpio_DiscreteWrite(&output,1 , 0xF);}
-			  else { XGpio_DiscreteWrite(&output,1,0x0);}
-	for (count = 0; count < delay; count++);
-	}
-return 0;
+	  case 3: // All 4 together
+		  all = !all;
+		  if (all) {
+		  XGpio_DiscreteWrite(&output,1 , 0xF);}
+		  else { XGpio_DiscreteWrite(&output,1,0x0);}
+		  for (count = 0; count < delay; count++);
+		  break;
+  	  }
+
+  }
+  return 0;
 }
 
 
