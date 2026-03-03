94867af399ad312bd7d4e254629d60932ddb3f3a
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-03-03 08:16:00 -0500

    Add files via upload

---
+++ Lab3.cc
@@ -0,0 +1,23 @@
+/*
+	----------------------------------------------------------------------
+	University at Buffalo, EE379, Spring 2020	
+	----------------------------------------------------------------------
+	File Name: Lab3.cc
+	Description: Strating point for Lab 3 code - include MTDS libraries.
+	
+	Revision History:
+	Date      By          Change
+	----------------------------------------------------------------------
+	20200707  cvfritz     Original
+
+*/
+
+#include <MyDisp.h>
+
+int main() {
+	MYDISP display;
+	display.begin();
+	
+	
+
+}
