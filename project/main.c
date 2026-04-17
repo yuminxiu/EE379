f3022473efe83750bf390212cbd52aad6ef315e9
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-17 14:30:10 -0400

    Update main.c

--- project/main.c
+++ project/main.c
@@ -1,1 +1,18 @@
+#include "xscugic.h"
+#include "xtmrctr.h"
+#include "xparameters.h"
+#include <XGpio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <MyDisp.h>
+#define clrPink 0x00FFC0CBul
 
+//Defines for interrupt IDs
+#define INTC_DEVICE_ID XPAR_PS7_SCUGIC_0_DEVICE_ID
+#define GPIO_INT_ID XPAR_FABRIC_AXI_GPIO_0_IP2INTC_IRPT_INTR
+#define TIMER_INT_ID XPAR_FABRIC_AXI_TIMER_0_INTERRUPT_INTR
+
+
+while (true) {
+
+}
