7b20e464473ba1fe8cb1396c9e5c5c0d94cf1894
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-17 14:27:26 -0400

    Create interrupts.c

---
+++ project/interrupts.c
@@ -0,0 +1,15 @@
+
+static XScuGic GIC;
+
+void initIntrSystem(XScuGic * IntcInstancePtr) {
+
+	XScuGic_Config *IntcConfig;
+	IntcConfig = XScuGic_LookupConfig(XPAR_PS7_SCUGIC_0_DEVICE_ID);
+	XScuGic_CfgInitialize(IntcInstancePtr, IntcConfig, IntcConfig->CpuBaseAddress);
+	Xil_ExceptionInit();
+	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT, (Xil_ExceptionHandler)XScuGic_InterruptHandler, (void *)IntcInstancePtr);
+	Xil_ExceptionEnable();
+
+}
+
+initIntrSystem(&GIC);
