3cfc8cbfd2820706b4577c537a429e0c7799ec4f
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-18 18:53:37 -0400

    Update test.c

--- project/test.c
+++ project/test.c
@@ -6,16 +6,16 @@
 
 int main() {
   struct Player player;
-  struct Bullet bullets[max_bullets];
+  struct Bullet bullets[MAX_BULLETS];
 
   init_player(&player);
-  init_bullet_arr(bullets, max_bullets);
+  init_bullet_arr(bullets, MAX_BULLETS);
 
   printf("Initial player position: %d, %d\n", player.pos_x, player.pos_y);
 
-  player_shoot(&player, bullets, max_bullets);
+  player_shoot(&player, bullets, MAX_BULLETS);
 
-  for (int i = 0; i < max_bullets; i++){
+  for (int i = 0; i < MAX_BULLETS; i++){
     if (bullets[i].active){
       printf("bullet spawned at: %d, %d \n", bullets[i].pos_x, bullets[i].pos_y);
     }
@@ -23,11 +23,11 @@ int main() {
 
  for (int frame = 0; frame < 5; frame++) {
         update_player(&player);
-        update_bullet_arr(bullets, max_bullets);
+        update_bullet_array(bullets, MAX_BULLETS);
 
         printf("Frame %d:\n", frame);
 
-        for (int i = 0; i < max_bullets; i++) {
+        for (int i = 0; i < MAX_BULLETS; i++) {
             if (bullets[i].active) {
                 printf(" Bullet %d at (%d, %d)\n",
                        i,
