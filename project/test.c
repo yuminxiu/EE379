ea073339b2448b9c912230796a5ca13b35c8df50
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-18 19:23:49 -0400

    Update test.c

--- project/test.c
+++ project/test.c
@@ -23,7 +23,7 @@ int main() {
 
  for (int frame = 0; frame < 5; frame++) {
         update_player(&player);
-        update_bullet_array(bullets, MAX_BULLETS);
+        update_bullet_arr(bullets, MAX_BULLETS);
 
         printf("Frame %d:\n", frame);
 
