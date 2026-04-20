d9e2b16e084e9fe827c64932cf237188b93a77e8
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-20 16:38:52 -0400

    Update powerup.h

---
+++ project/powerup.h
@@ -0,0 +1,28 @@
+#ifndef POWERUP_H
+#define POWERUP_H
+
+#include <stdbool.h>
+
+
+struct PowerUp {
+  bool active;
+
+  int type;
+
+  int pos_x;
+  int pos_y;
+
+  int dy;
+
+  int despawn_timer;
+  int spawn_timer;
+
+  bool landed; //stay on bottom edge of screen until despawn or collision with player
+};
+
+void init_powerup(struct PowerUp *p);
+void update_powerup(struct PowerUp *p);
+void powerup_move(struct PowerUp *p);
+
+#endif
+
