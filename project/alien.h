f8f831e9a8bd797586b10f540c811841b6662445
Amanda Rottenberg <amandarottenberg3@gmail.com>
2026-04-23 11:50:54 -0400

    Update alien.h

---
+++ project/alien.h
@@ -0,0 +1,24 @@
+#ifndef ALIEN_H
+#define ALINE_H
+
+#include <stdbool.h>
+
+#define MAX_ALIENS 18 // 3 rows of 6
+
+struct Alien {
+  bool active;
+
+  int pos_x;
+  int pos_y;
+  int row;
+  int col;
+
+  int sprite_type;
+};
+
+struct Alien_Formation {
+  int speed;
+  int direction; // -1 = left, +1 = right
+  int step_down;
+};
+
