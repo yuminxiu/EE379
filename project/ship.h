9c83c2963453276ac5657c1e351b8378488d0d18
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-27 18:28:29 -0400

    Update ship.h

--- project/ship.h
+++ project/ship.h
@@ -1,1 +1,24 @@
+#ifndef SHIP_H
+#define SHIP_H
+
+#include <stdbool.h>
+
+struct Ship {
+  bool active;
+
+  int pos_x;
+  int pos_y;
+  int dx;
+
+  int spawn_timer;
+
+  int direction; // 1 = left, 0 = right
+};
+
+void init_ship(struct Ship *s);
+void update_ship(struct Ship *s);
+void ship_move(struct Ship *s);
+void spawn_ship(struct Ship *s);
+
+#endif
 
