3f1acff529ca1880ed1db0494d2a7e79c2a26e51
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-27 22:17:39 -0400

    Update bullet.c

--- project/bullet.c
+++ project/bullet.c
@@ -62,6 +62,15 @@ void spawn_bullet(struct Bullet bullets[], int size,
             bullets[i].type = type;
             bullets[i].owner = owner;
             bullets[i].active = true;
+
+          if (type == BULLET_LASER){
+              bullets[i].dx= 0;
+              bullets[i].dy = PLAYER_BULLET_SPEED *2;
+          }
+
+          if (type == BULLET_PIERCE) {
+              bullets[i].dy = PLAYER_BULLET_SPEED;
+          }
             return;
         }
     }
