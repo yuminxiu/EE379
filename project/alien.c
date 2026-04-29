bd616df83031d779ac598c78cc6085476d841c45
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-29 19:53:59 -0400

    Update alien.c

--- project/alien.c
+++ project/alien.c
@@ -22,7 +22,7 @@ void init_alien_arr(struct Alien aliens[], int size){
   int start_x = 30;
   int start_y = ENEMY_START_Y;
 
-  for (int i=0, i <size, i++){
+  for (int i=0; i <size; i++){
     int row = i/ALIEN_COLS;
     int col = i % ALIEN_COLS;
 
@@ -37,3 +37,44 @@ void init_alien_arr(struct Alien aliens[], int size){
   }
 }
 
+void update_alien_arr(struct Alien aliens[], int size, struct Alien_Formation *f){
+  bool hit_edge = false;
+
+  for (int i = 0; i < size; i++){ // checking if next move would hit either side
+    if (!aliens-[i].active){
+      continue;
+    }
+
+    int next_x = aliens[i].pos_x + f->direction * f->speed;
+
+    if (next <= 00 || next_x + ALIEN_WIDTH >= SCREEN_WIDTH){
+      hit_edge = true;
+      break;
+    }
+  }
+
+    if (hit_edge){
+      f->direction *= -1;
+      for (int i=0; i<size; i++) {
+          if (aliens[i].active){
+              aliens[i].pos_y += f->step_down;
+          }
+      }
+    }
+
+    else { 
+      for int i = 0; i <size; i++){
+        if (aliens[i].active) {
+            aliens[i].pos_x += f->direction * f->speed;
+        }
+      }
+    }
+}
+
+void alien_shoot(struct Alien *alien, struct Bullet bullets[], int max_bullets) {
+      if (!alien->active) { 
+        return;
+      }
+
+    spawn_bullet(bullets, max_bullets, alien->pos_x + ALIEN_WIDTH, alien->pos_y + ALIEN_HEIGHT, 0, ENEMY_BULLET_SPEED, BULLET_NORMAL, OWNER_ALIEN);
+}
