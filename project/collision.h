baa7e3f9f16b76af5166f45d876de8efa500414b
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-29 15:31:05 -0400

    Update collision.h

---
+++ project/collision.h
@@ -0,0 +1,29 @@
+#ifndef COLLISION_H
+#define COLLISION_H
+
+#include <stdbool.h>
+
+#include "player.h"
+#include "bullet.h"
+#include "boss.h"
+#include "alien.h"
+#include "ship.h"
+#include "powerup.h"
+
+struct Rect {  // hitbox needed for collisions
+    int x;
+    int y;
+    int width;
+    int height;
+};
+
+bool rect_col(struct Rect a, struct Rect b);
+
+struct Rect player_rect(struct Player *player);
+struct Rect bullet_rect(struct Bullet *bullet);
+struct Rect boss_rect(struct Boss *boss);
+struct Rect powerup_rect(struct PowerUp *p);
+struct Rect alien_rect(struct Alien *alien);
+struct Rect ship_rect(struct Ship *s);
+
+#endif
