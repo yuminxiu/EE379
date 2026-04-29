3adb48898f84a64eb89b1bab620cbdec0751eb9d
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-29 17:35:13 -0400

    Update alien.h

--- project/alien.h
+++ project/alien.h
@@ -2,7 +2,7 @@
 #define ALIEN_H
 
 #include <stdbool.h>
-#include "bullets.h"
+#include "bullet.h"
 
 #define MAX_ALIENS 18 // 3 rows of 6
 
@@ -24,8 +24,11 @@ struct Alien_Formation {
 };
 
 void init_alien(struct Alien *alien);
-void move_alien(struct Alien *alien, struct Alien_Formation *f);
+void init_alien_arr(struct Alien aliens[], int size);
+void update_alien_arr(struct Alien aliens[], int size, struct Alien_Formation *f);
 void update_alien(struct Alien *alien, struct Alien_Formation *f);
 void alien_shoot(struct Alien *alien, struct Bullet bullets[], int max_bullets);
+
+void move_alien(struct Alien *alien, struct Alien_Formation *f); // not currently used
 
 #endif
