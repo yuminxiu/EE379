e9fcee387a5c10d392be4e0a08c00537d81ce4e4
Amanda Rottenberg <amandarottenberg3@gmail.com>
2026-04-30 10:18:52 -0400

    Update timer.h

---
+++ project/timer.h
@@ -0,0 +1,27 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <stdbool.h>
+#include "timer.h"
+
+struct Timer {
+  bool active;
+  int sec;
+  int max_hp;
+  int 
+  int width;
+  int height;
+  int pos_x;
+  int pos_y;
+  int dx;
+  int dy;
+
+  int pattern_state;
+};
+
+void
+void
+void
+void
+
+#endif
