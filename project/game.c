180a131f77d3efe3a12d55e6f722d4fe4786d44a
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-29 21:16:23 -0400

    Update game.c

--- project/game.c
+++ project/game.c
@@ -1,3 +1,17 @@
+#include "game.h"
+
+#include "constants.h"
+#include "player.c"
+#include "score.c"
+#include "boss.c"
+#include "alien.c"
+#include "render.c"
+#include "powerup.c"
+#include "collision.c"
+#include "ship.c"
+#include "bullet.c"
+
+
 
 int main() {
 
