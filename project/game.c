72bf863e1a576f69251d6b0c9b200b78c910fbea
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-30 19:04:29 -0400

    Update game.c

--- project/game.c
+++ project/game.c
@@ -1,21 +1,47 @@
 #include "game.h"
 
 #include "constants.h"
-#include "player.c"
-#include "score.c"
-#include "boss.c"
-#include "alien.c"
-#include "render.c"
-#include "powerup.c"
-#include "collision.c"
-#include "ship.c"
-#include "bullet.c"
+#include "player.h"
+#include "score.h"
+#include "boss.h"
+#include "alien.h"
+#include "render.h"
+#include "powerup.h"
+#include "collision.h"
+#include "ship.h"
+#include "bullet.h"
+#include "input.h"
+
+struct Game_State game;
+struct Player player;
+struct Boss boss;
+struct Bullet bullets[MAX_BULLETS]
+struct Alien aliens[MAX_ALIENS]
+struct Alien_Formation f;
+struct Ship ship;
+struct PowerUps powerups[MAX_POWERUPS];
+struct Score_Sys sc;
+struct Input input;
 
 
 
-int main() {
+void init_game(void){
+  game.mode = MODE_START;
+  game.boss_stage = false;
+  game.wave = 1;
 
+  game.timers.frame_count = 0;
+  game.timers.seconds_count = 0;
+  game.timers.event_timer = 0;
+  game.timers.powerup_spawn_timer = 0;
 
-
-
+  init_player(&player);
+  init_alien_formation(&f);
+  init_alien_arr(aliens, MAX_ALIENS);
+  init_bullet_arr(bullets, MAX_BULLETS);
+  init_ship(&ship);
+  init_score_sys(&sc);
+  init_boss(&boss);
+  init_powerup_arr(powerups, MAX_POWERUPS);
+  init_input(&input);
 }
