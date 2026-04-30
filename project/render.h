29afb2a4413d0c06efb8ba150b3165e2432c2c7c
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-29 21:26:38 -0400

    Update render.h

---
+++ project/render.h
@@ -0,0 +1,25 @@
+#ifndef RENDER_H
+#define RENDER_H
+
+#include "player.h"
+#incldue "bullet.h"
+#include "alien.h"
+#include "boss.h"
+#include "powerup.h"
+#include "score.h"
+#include "game.h"
+
+void init_render(void);
+
+void render_game(struct Game_State *game, struct Player *player, struct PowerUp powerups[],int max_powerups, struct Ship *ship, struct Alien *aliens[3][6],int rows, int cols,
+    struct Score_Sys *sc, struct Boss *boss, struct Bullet bullets[], int max_bullets);
+
+void draw_player(struct Player *player);
+void draw_bullets(struct Bullet bullets[], int max_bullets);
+void draw_ship(struct Ship *ship);
+void draw_aliens(struct Alien *aliens[3][6], int rows, int cols);
+void draw_boss(struct Boss *boss);
+void draw_powerups(struct PowerUp powerups[], int max_powerups);
+void draw_ui(struct Player *player, struct Score_Sys *sc, Game_State *game);
+
+#endif
