2964facde0b061af5cbf4ec79348ec9d7ae5307d
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-30 14:57:29 -0400

    Update game_timer.h

--- project/game_timer.h
+++ project/game_timer.h
@@ -1,32 +1,20 @@
 #ifndef GAMETIMER_H
 #define GAMETIMER_H
 
+#include "xscugic.h"
+#include "xtmrctr.h"
+#include "xil_types.h"
+
+
+#define TIMER_INT_ID  XPAR_FABRIC_AXI_TIMER_0_INTERRUPT_INTR
+
+
 #include <stdbool.h>
-#include "timer.h"
 
-struct GameTimer {
-  bool active;
-  int sec;
-  int max_hp;
-  int 
-  int width;
-  int height;
-  int pos_x;
-  int pos_y;
-  int dx;
-  int dy;
+extern XTmrCtr timer;
+extern volatile int timer_tick = 0;
 
-  int pattern_state;
-};
-
-void init_timer(struct GameTimer *timer);
-void init_timer_arr(struct GameTimer seconds[], int size);
-
-void update_timer(struct GameTimer *timer);
-void update_timer_arr(struct GameTimer seconds[], int size);
-
-void deactivate_timer(struct GameTimer *timer);
-
-void activate_timer(struct GameTimer seconds[], int size, int x, int y, int dx, int dy, int type, int owner);
+void init_game_timer(XScuGic *GIC);
+void timerInterruptHandler(void *userParam, u8 TmrCtrNumber);
 
 #endif
