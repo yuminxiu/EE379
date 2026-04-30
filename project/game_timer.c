0b1440b7f0ec8919594e3bda480387f0d0cfa820
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-30 15:07:59 -0400

    Update game_timer.c

--- project/game_timer.c
+++ project/game_timer.c
@@ -1,4 +1,5 @@
 #include "game_timer.h"
+#define FPS 60
 
 volatile int timer_tick = 0;
 
@@ -11,12 +12,13 @@ void timerInterruptHandler(void *userParam, u8 TmrCtrNumber) {
 
 }
 
-	//Configure Timer and timer interrupt as done in class, and comment every line
+void init_game_timer(XScuGic *GIC){
+	
 	XTmrCtr_Initialize (&timer, XPAR_AXI_TIMER_0_DEVICE_ID);
 	XTmrCtr_SetHandler(&timer, ( XTmrCtr_Handler ) timerInterruptHandler, ( void*) 0x12345678 );
 	XScuGic_Connect (&GIC, TIMER_INT_ID, ( Xil_InterruptHandler ) XTmrCtr_InterruptHandler, &timer);
 	XScuGic_Enable (&GIC, TIMER_INT_ID );
 	XScuGic_SetPriorityTriggerType (&GIC, TIMER_INT_ID, 0x0, 0x3 );
 	XTmrCtr_SetOptions (&timer, 0, XTC_INT_MODE_OPTION | XTC_AUTO_RELOAD_OPTION);
-	XTmrCtr_SetResetValue (&timer, 0, 0xFFFFFFFF - 66666666); // 1 Hz
+	XTmrCtr_SetResetValue (&timer, 0, 0xFFFFFFFF - (100000000/FPS); // 60 fps. 3333333 for 30 fps
 	XTmrCtr_Start(&timer, 0);
