9c90bb3338a17514a33cac18382b18ac0576fa0d
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-30 10:03:56 -0400

    Update bossbeam.h

--- project/bossbeam.h
+++ project/bossbeam.h
@@ -1,1 +1,20 @@
+#ifndef BOSSBEAM_H
+#define BOSSBEAM_H
 
+#include <stdbool.h>
+
+struct BossBeam{
+  bool active;
+  bool firing;
+  int x;
+  int y;
+  int width;
+  int height;
+
+  int warning_timer; 
+  int fire_timer;
+};
+
+void init_boss_beam(struct BossBeam *t);
+void init_boss_beam_arr(struct BossBeam beams[], int max_beams);
+void beam_cage(struct BossBeam beams
