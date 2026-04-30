efcb2918f134267e29625887d27419e3afb194f8
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-04-30 11:49:26 -0400

    Update bossbeam.h

--- project/bossbeam.h
+++ project/bossbeam.h
@@ -18,9 +18,9 @@ struct BossBeam{
 void init_boss_beam(struct BossBeam *b);
 void init_boss_beam_arr(struct BossBeam beams[], int size);
 void start_beam_cage(struct BossBeam beams[], int size);
-void update_boss_beam(struct BossBeam *beam);
+void update_boss_beam(struct BossBeam *b);
 void update_boss_beam_arr(struct BossBeam beams[], int size);
 
-void deactivate_boss_beam(struct BossBeam *beam);
+void deactivate_boss_beam(struct BossBeam *b);
 
 #endif
