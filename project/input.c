f92cb32f85c435e5ea35151857ff0ba7594544da
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-05-01 16:24:24 -0400

    Update input.c

--- project/input.c
+++ project/input.c
@@ -1,12 +1,14 @@
 #include "input.h"
 #include "xuartps.h"
 #include "xparameters.h"
+#include "xgpio.h"
 
 #define UART_BASEADDR XPAR_XUARTPS_0_BASEADDR
 // XPAR_PS7_UART_1_BASEADDR
 
 const char cheat_code[]= "wwssadadqe ";
 int cheat_index = 0;
+XGpio button;
 
 void init_input(struct Input *input){
 	input->left = false;
@@ -18,6 +20,9 @@ void init_input(struct Input *input){
 	input->restart = false;
 	input->key_pressed = 0;
 	input->cheat = false;
+
+	XGpio_Initialize(&button, XPAR_AXI_GPIO_0_DEVICE_ID);
+	XGpio_SetDataDirection(&button, 1, 0xF); //1 = input, 0 = output
 }
 
 void update_input(struct Input *input){
@@ -69,7 +74,7 @@ void update_input(struct Input *input){
 		if (c == cheat_code[cheat_index]){
 			cheat_index++;
 
-			if (cheat_code[cheat-index= == '\0'){
+			if (cheat_code[cheat_index] == '\0'){
 				input->cheat = true;
 				cheat_index = 0;
 			}
@@ -78,32 +83,30 @@ void update_input(struct Input *input){
 			}
 			
 		}								
-																																			
+
+		int buttons = XGpio_DiscreteRead(&button,1);
+
+		if (buttons & 0x1) input->left = true;
+		if (buttons & 0x2) input->right = true;
+		if (buttons & 0x4) input->shoot = true;
+		if (buttons & 0x8) input->pause = true;
 }
 
 
-
-
-
-
-
-// TAKEN FROM LAB 5. UNSURE IF NEEDED AT ALL, BUT MAY NEED IF UART ISNT ALREADY ENABLED ON THE BOARD
-XGpio input;
-
-void buttonInterruptHandler(void *instancePointer) {
+*/ void buttonInterruptHandler(void *instancePointer) {
 	//Read the button state using XGpio_DiscreteRead
-	//Set dir based on which bit is a '1'
-	switch_data = XGpio_DiscreteRead(&input,1);
+	//Set dir based on which bit is a '1';
 
 
-	XGpio_InterruptClear(&input, 0xF); //Leave this line at the end of this function
+	XGpio_InterruptClear(&button, 0xF); //Leave this line at the end of this function
 
 }
 
-XGpio_Initialize(&input, XPAR_AXI_GPIO_0_DEVICE_ID);
-XGpio_SetDataDirection(&input, 1, 0xF); //1 = input, 0 = output
-XScuGic_Connect(&GIC, GPIO_INT_ID, (Xil_ExceptionHandler) buttonInterruptHandler, &input);
-XGpio_InterruptEnable (&input, XGPIO_IR_CH1_MASK);
-XGpio_InterruptGlobalEnable (&input);
+XGpio_Initialize(&button, XPAR_AXI_GPIO_0_DEVICE_ID);
+XGpio_SetDataDirection(&button, 1, 0xF); //1 = input, 0 = output
+XScuGic_Connect(&GIC, GPIO_INT_ID, (Xil_ExceptionHandler) buttonInterruptHandler, &button);
+XGpio_InterruptEnable (&button, XGPIO_IR_CH1_MASK);
+XGpio_InterruptGlobalEnable (&button);
 XScuGic_Enable (&GIC, GPIO_INT_ID);
 XScuGic_SetPriorityTriggerType (&GIC, GPIO_INT_ID, 0x8, 0x3);
+/*
