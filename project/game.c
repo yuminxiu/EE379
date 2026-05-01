49cedc2f367565b49289eebc46978152faf08f3d
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-05-01 00:07:47 -0400

    Update game.c

--- project/game.c
+++ project/game.c
@@ -18,6 +18,7 @@ static void handle_input_playing(void);
 static void update_entities(void);
 static void handle_spawning(void);
 static void handle_collisions(void);
+static void clear_wave_check(void);
 
 struct Game_State game;
 struct Player player;
@@ -30,6 +31,7 @@ struct PowerUp powerups[MAX_POWERUPS];
 struct Score_Sys sc;
 struct Input input;
 
+#define MAX_WAVE 3
 
 
 void init_game(void){
@@ -70,20 +72,6 @@ void update_game(void){
         game.mode = MODE_PAUSED;
         return;
       }
-    
-
-    update_player(&player);
-    update_bullet_arr(bullets, MAX_BULLETS);
-    update_powerup_arr(powerups, MAX_POWERUPS);
-
-    if (game.boss_stage){
-      update_boss(&boss);
-      boss_shoot(&boss, bullets, MAX_BULLETS);
-    } else {
-      update_alien_arr(aliens, MAX_ALIENS);
-      update_ship(&ship);
-    }
-
   }
 
 
@@ -148,15 +136,80 @@ static void update_timers(void){
       game.timers.event_timer++;
 
     if (game.timers.frame_count >= GAME_FPS){
+      game.timers.frame_count = 0;
       game.timers.seconds_count++;
     }
 
 }
+
 static void handle_input_playing(void){
+  if(input.left){
+    player_move(&player, -PLAYER_SPEED);
+  }
+
+  if(input.right){
+    player_move(&player, PLAYER_SPEED);
+  }
+
+  if(input.shoot){
+    player_shoot(&player, bullets, MAX_BULLETS);
+  }
 }
+
 static void update_entities(void){
+    update_player(&player);
+    update_bullet_arr(bullets, MAX_BULLETS);
+    update_powerup_arr(powerups, MAX_POWERUPS);
+
+    if (game.boss_stage){
+      update_boss(&boss);
+      boss_shoot(&boss, bullets, MAX_BULLETS);
+    } else {
+      update_alien_arr(aliens, MAX_ALIENS);
+      alien_shoot(&alien, bullets, MAX_BULLETS);
+      update_ship(&ship);
+    }
 }
 static void handle_spawning(void){
+  if(game.timers.powerup_spawn_timer > 300){
+    int type = (rand() % 5) + 1;
+    spawn_powerup(powerups, MAX_POWERUPS,p.pos_x, p.pos_y, type, POWERUP_SPEED, POWERUP_DESPAWN_TIME);
+    game.timers.powerup_spawn_timer = 0;
+  }
+
+  if(game.timers.event_timer == 450){ //this is a placeholder value because im unsure what else will be determined by event_timer
+    //also unsure if spawn_ship should be triggered by event_timer or seconds_count
+    int direction;
+    if(direction == (rand() % 2 == 0)){
+      direction = 1;
+    }
+    spawn_ship(&ship, direction);
+    game.timers.event_timer = 0;
+  }
 }
 static void handle_collisions(void){
+}
+static void clear_wave_check(void){
+  handle_collisions();
+  if(*/all aliens dead*/){
+    wave++;
+    init_alien_arr(aliens, MAX_ALIENS);
+    
+    if(wave == 3){
+      game.boss_stage = true;
+    }
+  }
+  
+  if (game.boss_stage){
+    if (boss.hp <= 0){
+      printf("WINNER!");
+    if (qualify_high_score(&sc,sc.current_score)){
+      game.mode = MODE_ENTER_INITIALS;
+      return;
+    } else {
+      game.mode = MODE_SCOREBOARD;
+    }
+    return;
+    }
+  }
 }
