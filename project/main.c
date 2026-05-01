863bb3254329a53d852c9a811d2ec672ee273bba
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-05-01 16:09:24 -0400

    Update main.c

--- project/main.c
+++ project/main.c
@@ -1,24 +1,37 @@
 #include "xscugic.h"
 #include "xtmrctr.h"
 #include "xparameters.h"
+#include "interrupts.h"
+#include "game_timer.h"
+#include "game.h"
+#include "render.h"
+
 #include <XGpio.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <MyDisp.h>
-#include "interrupts.h"
-#include "game_timer.h"
-
-#define clrPink 0x00FFC0CBul
 
 //Defines for interrupt IDs
 #define INTC_DEVICE_ID XPAR_PS7_SCUGIC_0_DEVICE_ID
 #define GPIO_INT_ID XPAR_FABRIC_AXI_GPIO_0_IP2INTC_IRPT_INTR
 #define TIMER_INT_ID XPAR_FABRIC_AXI_TIMER_0_INTERRUPT_INTR
 
-int main(){
+int main(void){
   initIntrSystem(&GIC);
   init_game_timer(&GIC);
 
-while (true) {
+  init_input(&input);
+  init_render();
+  init_game();
 
+  while (true) {
+    if (timer_tick){
+      timer_tick = 0;
+  
+      update_game();
+      render_game();
+    }
+  }
+  return 0;
 }
