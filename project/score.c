5f4cfbc640bc75f6fdf9c54730c704356dc3e4a4
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-05-03 14:46:31 -0400

    Update score.c

--- project/score.c
+++ project/score.c
@@ -2,10 +2,10 @@
 #include "constants.h"
 
 
-void init_score_sys(struct Score_Sys *sc}(
+void init_score_sys(struct Score_Sys *sc){
   sc->current_score = 0;
 
-  for (i = 0; i < MAX_SCORES; i++){
+  for (int i = 0; i < MAX_SCORES; i++){
       sc->top_scores[i].initials[0] = '-';
       sc->top_scores[i].initials[1] = '-';
       sc->top_scores[i].initials[2] = '-';
@@ -22,7 +22,7 @@ void add_score(struct Score_Sys *sc, struct Player *player, int points){
     points *= SCORE_MULT;
   }
 
-  sc->current_score += points
+  sc->current_score += points;
 }
 
 bool qualify_high_score(struct Score_Sys *sc, int score){
@@ -30,7 +30,8 @@ bool qualify_high_score(struct Score_Sys *sc, int score){
 }
 
 int high_score_rank(struct Score_Sys *sc, int score){
-  for (i = 0; i < MAX_SCORES; i++){
+
+  for (int i = 0; i < MAX_SCORES; i++){
     if (score > sc->top_scores[i].score){
       return i;
     }
@@ -45,7 +46,8 @@ if (rank == -1) {
   return;
 }
 
-for (i= MAX_SCORES - 1; i>rank; i--) {
+
+for (int i= MAX_SCORES - 1; i>rank; i--) {
   sc->top_scores[i] = sc -> top_scores[i-1];
 
     sc->top_scores[rank].score = score;
@@ -56,3 +58,4 @@ for (i= MAX_SCORES - 1; i>rank; i--) {
     sc->top_scores[rank].initials[3] = '\0';
 }
   
+}
