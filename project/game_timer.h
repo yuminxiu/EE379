00c428e331744f6246d3f735341d1a9ffbef95bd
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-05-03 18:11:59 -0400

    Update game_timer.h

--- project/game_timer.h
+++ project/game_timer.h
@@ -4,6 +4,7 @@
 #include "xscugic.h"
 #include "xtmrctr.h"
 #include "xparameters.h"
+#include "interrupts.h"
 
 
 #define TIMER_INT_ID  XPAR_FABRIC_AXI_TIMER_0_INTERRUPT_INTR
@@ -14,7 +15,7 @@
 extern XTmrCtr timer;
 extern volatile int timer_tick;
 
-void init_game_timer(XScuGic *GIC);
+void init_game_timer();
 void timerInterruptHandler(void *userParam, u8 TmrCtrNumber);
 
 #endif
