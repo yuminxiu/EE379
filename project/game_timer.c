03a0e88e9af0b253179cdd57e6b8dc598d3bf0a1
yuminxiu <88397943+yuminxiu@users.noreply.github.com>
2026-05-03 14:41:35 -0400

    Update game_timer.c

--- project/game_timer.c
+++ project/game_timer.c
@@ -4,7 +4,7 @@
 volatile int timer_tick = 0;
 
 XTmrCtr timer;
-
+XScuGic GIC;
 void timerInterruptHandler(void *userParam, u8 TmrCtrNumber) {
 
 	timer_tick = 1; // flag for main loop
@@ -12,7 +12,7 @@ void timerInterruptHandler(void *userParam, u8 TmrCtrNumber) {
 
 }
 
-void init_game_timer(XScuGic *GIC){
+void init_game_timer(XScuGic GIC){
 	
 	XTmrCtr_Initialize (&timer, XPAR_AXI_TIMER_0_DEVICE_ID);
 	XTmrCtr_SetHandler(&timer, ( XTmrCtr_Handler ) timerInterruptHandler, ( void*) 0x12345678 );
@@ -21,4 +21,5 @@ void init_game_timer(XScuGic *GIC){
 	XScuGic_SetPriorityTriggerType (&GIC, TIMER_INT_ID, 0x0, 0x3 );
 	XTmrCtr_SetOptions (&timer, 0, XTC_INT_MODE_OPTION | XTC_AUTO_RELOAD_OPTION);
 	XTmrCtr_SetResetValue (&timer, 0, TIMER_RESET_VALUE); // defined in constants.h
-	XTmrCtr_Start(&timer, 0);
+
+}
